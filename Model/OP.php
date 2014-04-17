27b6aa04c1d9e9ff8b1c2e3ffff1bdd19d4e745a
faceair <faceair.zh@gmail.com>
2014-04-17 22:28:06 +0800

    view model use direct variable

--- Model/OP.php
+++ Model/OP.php
@@ -2,12 +2,15 @@
 defined('INAF') or exit('Access Denied');
 class OP{
     public function view($path, $data = ''){
-        if(file_exists(AFROOT . '/View/' . $path . '.php')){
-            include(AFROOT . '/View/' . $path . '.php');
-        }else{
-            $data = array('msg'=>$path . ' Not Found.');
-            include(AFROOT . '/View/error/404.php');
+        if(!empty($data)){
+            foreach ($data as $key => $value)
+                $$key = $value;
         }
+
+        if(file_exists(AFROOT . '/View/' . $path . '.php'))
+            include(AFROOT . '/View/' . $path . '.php');
+        else
+            die('Not Found Template ' . AFROOT . '/View/' . $path . '.php');
     }
 }
 
