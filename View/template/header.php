c3d669f7206fd6ef7c9e4345b0e7a6ff2b1d08fa
faceair <faceair.zh@gmail.com>
2014-04-22 13:42:11 +0800

    remove bootstrap css

--- View/template/header.php
+++ View/template/header.php
@@ -4,8 +4,9 @@
     <title>知乎日报 - 满足你的好奇心</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <link rel="stylesheet" href="/Static/js/bootstrap.min.css">
     <?php
+    if($type == 'day')
+        print '<link rel="stylesheet" href="/Static/js/bootstrap.min.css">';
     print "<link rel=\"stylesheet\" href=\"/Static/css/{$type}.css\">";
     ?>
     <base target="_blank">
