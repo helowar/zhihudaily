78c717ecbe9b1cdcf746117266ae2f93ef25f386
faceair <faceair.zh@gmail.com>
2014-08-05 13:43:29 +0800

    refactor

--- View/error/404.php
+++
@@ -1,8 +0,0 @@
-<?php header("HTTP/1.1 404 Not Found");?>
-<html>
-<head><title>404 Not Found</title></head>
-<body bgcolor="white">
-<center><h2>404 Not Found.</h2></center>
-<hr><center>zhihudaily</center>
-</body>
-</html>
